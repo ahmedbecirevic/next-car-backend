e223d9c05a12607a37313954d3472c691bf4ef05
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2022-03-26 11:32:07 +0100

    add config for env variables

---
+++ config.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import logger from './logger.js';
+
+dotenv.config();
+
+const envVar = process.env;
+
+export default {
+  NODE_ENV: envVar.NODE_ENV,
+  PORT: envVar.PORT,
+};
+
+export const validateEnvironmentVariables = () => {
+  const environmentVariables = [
+    "NODE_ENV",
+    "PORT"
+  ];
+  const missingEnvironmentVariables = [];
+
+  environmentVariables.forEach((environmentVariable) => {
+    if (!process.env[environmentVariable]) {
+      missingEnvironmentVariables.push(environmentVariable);
+    }
+  });
+
+  if (missingEnvironmentVariables.length) {
+    throw new Error(`❗ Missing environment variables:\n${missingEnvironmentVariables.join('\n')}`);
+  }
+  logger.info('Environment successfully configured!');
+};
