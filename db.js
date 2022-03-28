e2813207459b31ea502a5d641cf168bfa8c5496c
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2022-03-28 17:41:16 +0200

    add db conn

---
+++ db.js
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+import config from './config.js';
+import logger from './logger.js';
+
+const sequelize = new Sequelize({
+  host: config.DB_HOST,
+  database: config.DB_NAME,
+  username: config.DB_USER,
+  password: config.DB_HOST_PASSWORD,
+  dialect: config.DB_DIALECT,
+});
+
+export const testConnection = async () => {
+  try {
+    if (config.NODE_ENV === 'test') {
+      return;
+    }
+    await sequelize.authenticate();
+    logger.info('Connection has been established successfully.');
+  } catch (error) {
+    logger.error('Unable to connect to the database:', error);
+  }
+};
+
+export default sequelize;
