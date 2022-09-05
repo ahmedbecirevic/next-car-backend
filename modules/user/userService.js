d6cb00adf8e1725ab482aaeba4500c6b4561ca7a
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2022-09-05 19:36:51 +0200

    add id to when user fetching

--- modules/user/userService.js
+++ modules/user/userService.js
@@ -1,12 +1,12 @@
 import { getUserById } from './userDal.js';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getUserDetails = async (id) => {
+export const getUserDetails = async (userId) => {
   const {
-    email, profilePictureUrl, phoneNumber, createdAt,
-  } = await getUserById(id) || {};
+    email, profilePictureUrl, phoneNumber, createdAt, id,
+  } = await getUserById(userId) || {};
 
   return {
-    email, profilePictureUrl, phoneNumber, createdAt,
+    email, profilePictureUrl, phoneNumber, createdAt, id,
   };
 };
